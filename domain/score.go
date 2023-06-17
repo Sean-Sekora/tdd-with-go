2dc133db9cc43d17f245d972df252880fa650f6c
Sekora <sean@sekora.io>
2023-06-17 17:16:37 -0400

    refactor

--- domain/score.go
+++ domain/score.go
@@ -12,16 +12,16 @@ type Score struct {
 
 func (s *Score) assess(attempt string) {
 	for i, character := range attempt {
-		s.Position = i
-		s.Results = append(s.Results, s.scoreFor(character))
+		(*s).Position = i
+		(*s).Results = append((*s).Results, (*s).scoreFor(character))
 	}
 }
 
 func (s *Score) scoreFor(character rune) Letter {
 	switch {
-	case s.isCorrect(character):
+	case (*s).isCorrect(character):
 		return CORRECT
-	case s.occursInWord(character):
+	case (*s).occursInWord(character):
 		return PART_CORRECT
 	default:
 		return INCORRECT
@@ -29,9 +29,9 @@ func (s *Score) scoreFor(character rune) Letter {
 }
 
 func (s *Score) isCorrect(attempt rune) bool {
-	return strings.IndexRune(string(s.CorrectWord), attempt) == s.Position
+	return strings.IndexRune(string((*s).CorrectWord), attempt) == (*s).Position
 }
 
 func (s *Score) occursInWord(attempt rune) bool {
-	return strings.ContainsRune(string(s.CorrectWord), attempt)
+	return strings.ContainsRune(string((*s).CorrectWord), attempt)
 }
